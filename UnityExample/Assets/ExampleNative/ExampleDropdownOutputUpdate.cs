d9c625af5a1f176ed7e3462714d6f6ca7cdb3a1b
Steve Halliwell <steve@animaginedreality.com>
2021-09-14 11:18:59 +1000

    Add dropdown to test scene

---
+++ UnityExample/Assets/ExampleNative/ExampleDropdownOutputUpdate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleDropdownOutputUpdate : MonoBehaviour
+{
+    [SerializeField] private Text ouputText;
+    [SerializeField] private Dropdown inDropdown;
+
+    private void Start()
+    {
+        inDropdown.onValueChanged.AddListener(SetTextFromDropdown);
+    }
+
+    private void SetTextFromDropdown(int index)
+    {
+        ouputText.text = inDropdown.options[index].text;
+    }
+}
